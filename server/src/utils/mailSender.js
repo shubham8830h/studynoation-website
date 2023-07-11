f23ffcfcbdcf42a36735bc5723dc491f6d0bbd1d
shubham8830h <shubhamhanamane1111@gmail.com>
2023-07-11 15:41:23 +0530

    bug free mail function

--- server/src/utils/mailSender.js
+++ server/src/utils/mailSender.js
@@ -1,47 +1,32 @@
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
 require("dotenv").config();
-
 const mailSender = async (email, title, body) => {
+  console.log(process.env.MAIL_HOST);  console.log(process.env.MAIL_USER);
+  console.log(process.env.MAIL_PASS);
+
   try {
-    const oAuth2Client = new google.auth.OAuth2(
-      process.env.OAUTH_CLIENT_ID,
-      process.env.OAUTH_CLIENT_SECRET,
-      "https://developers.google.com/oauthplayground"
-    );
-
-    oAuth2Client.setCredentials({
-      access_token: process.env.OAUTH_ACCESS_TOKEN,
-      refresh_token: process.env.OAUTH_REFRESH_TOKEN,
-    });
-
-    const accessToken = await oAuth2Client.getAccessToken();
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: 587,
       auth: {
-        type: "OAuth2",
-        user: "sh9422602@gmail.com",
-        clientId: process.env.OAUTH_CLIENT_ID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
     let info = await transporter.sendMail({
-      from: "StudyNotion web site",
+      from: "StudyNotion || CodeHelp - by Babbar",
       to: `${email}`,
       subject: `${title}`,
       html: `${body}`,
     });
-
     console.log(info);
-
     return info;
   } catch (error) {
-    console.log("Error occurred while sending email:", error);
-    throw error;
+    console.log(error.message);
   }
 };
 
